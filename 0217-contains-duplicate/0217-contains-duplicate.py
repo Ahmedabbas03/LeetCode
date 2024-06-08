65dbd368c5e48fcbc4d3acea2a5f2bf6165c297d
Ahmed Abbas <86217781+Ahmedabbas03@users.noreply.github.com>
2024-06-07 19:14:44 -0500

    Time: 403 ms (91.94%), Space: 31.9 MB (59.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,37 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
-        # 1. Understand: 
-          # Can the input array be empty? -> NO
-        # 2. Match:
-          # Storing the elements of the array in a HashMap or a Set.
-          # If the number is already is in the set, return true otherwise false
-        # 3. Plan
-            # 1. Create a set to store number
-            # 2. Iterate thorugh numbers
-                # if number is already in set return True
-                # else store number in set
-            # return False (reached the end of list without duplicates)
-        # 4. Implement 
         
-        # Create set
-        hashSet = set()
-    
-        # Iterate thorugh numbers
+        # Create hashset
+        # Check if number in hashset, if it is then we already since them and we can return true
+        # else add to the hashset
+        # return false 
+        
+        
+        seen = set()
+        
         for num in nums:
-            if num in hashSet:
+            if num in seen:
                 return True
-            # store the number in set
-            hashSet.add(num)
-
-        # Return flase since we reached end of list wihtout duplicates
+            else:
+                seen.add(num)
+                
         return False
-
-
-    # Review:
-        # No issues,code passed
-    # Evaluate
-        # Time Complexity: O(N) because we need to traverse all numbers in the array.
-        # Space Complexity: O(N) because we may need to store all numbers in the array.
-
         
